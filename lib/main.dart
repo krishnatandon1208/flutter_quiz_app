93adcdd40806ba794a8e0170fdde2aa9e1f8103b
Krishna Tandon <krishnatandon.1208@gmail.com>
2020-08-24 19:12:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "Which mobile is best in 2020?",
+      "Which is the fastest animal?"
+    ];
+
+    return (MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Text(
+                      //questions.elementAt(0),
+                      questions[questionIndex],
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Answer 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: answerQuestion,
+                    color: Colors.red,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Answer 2",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      print("Answer 2 selected");
+                    },
+                    color: Colors.amber,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Answer 3",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      print("Answer 3 selected");
+                    },
+                    color: Colors.green,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Answer 4",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      print("Answer 4 selected");
+                    },
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ), //header
+    )); //Parent container/master block
+  }
+}
